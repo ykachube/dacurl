b6a55ae6e7bad25e4f1a19a96410f7eeb9dfbdea
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2022-11-16 11:23:53 +0300

    add errorlevel

--- dacurl.js
+++ dacurl.js
@@ -6,4 +6,5 @@ request(process.argv[2],  function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
  // console.log('body:', body); // Print the HTML for the Google homepage.
+  if (error) { process.exit(1);  }
 });
