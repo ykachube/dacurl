d06be9b87251c947ad738fd3781e8ad8929c49c7
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2022-12-19 12:38:48 +0300

    1.0.3 version - add body option, default only status code out

--- dacurl.js
+++ dacurl.js
@@ -3,8 +3,8 @@ const request = require('request');
 
 console.log(process.argv[2])
 request(process.argv[2],  function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- // console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.error('error:', error); // Print the error if one occurred
+  console.log( response && response.statusCode); // Print the response status code if a response was received
+  if (process.argv[3] && process.argv[3] == "body" ) { console.log('body:', body); }// Print the HTML for the Google homepage.
   if (error) { process.exit(1);  }
 });
