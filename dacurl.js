62b08215bc5ac50bb9890362d2fc0a7dc9ec3765
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2022-12-19 13:45:57 +0300

    1.0.4  version - return only code

--- dacurl.js
+++ dacurl.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const request = require('request');
 
-console.log(process.argv[2])
+//console.log(process.argv[2])
 request(process.argv[2],  function (error, response, body) {
   //console.error('error:', error); // Print the error if one occurred
   console.log( response && response.statusCode); // Print the response status code if a response was received
