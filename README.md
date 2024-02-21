28bfad2dd54bdd626ed638554acb0e209517ea65
Yury Kachubeyeu <yury_kachubeyeu@greensky.com>
2024-02-21 10:14:54 +0300

    1.0.6 - add command line options

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ simple curl implementation for npx
 usage:
 npm i dacurl
 npx dacurl https://google.com
+
+npx dacurl http://google.com   false false code      
+npx dacurl http://google.com   body error code    
+npx dacurl http://google.com   body error code  verbose
+npx dacurl http://google.com   body error code  verbose POST
+
+
--- dacurl.js
+++ dacurl.js
@@ -1,10 +1,40 @@
 #!/usr/bin/env node
 const request = require('request');
 
-//console.log(process.argv[2])
-request(process.argv[2],  function (error, response, body) {
-  console.log( response && response.statusCode); // Print the response status code if a response was received
-  if (process.argv[3] && process.argv[3] == "body" ) { console.log('body:', body); }// Print the HTML for the Google homepage.
-  if (process.argv[4] && process.argv[4] == "error" ) { console.error('error:', error); } // Print the error if one occurred
-  if (error) { process.exit(1);  }
+// Parse the command line arguments
+const [,,url, showBody, showErrors, getResponseCode, verbose, method = 'GET', headersJSON = '{}'] = process.argv;
+
+// Create an options object for the request
+const options = {
+  url, 
+  method,
+  headers: JSON.parse(headersJSON)
+};
+
+// Show debug info
+if (verbose=='verbose') {
+console.log('Using options:', options);
+}
+
+// Make the request
+request(options, function (error, response, body) {
+
+  if (verbose=='verbose') {
+   console.log('Response:', response && response.statusCode); 
+  }
+  //console.log('Status code:', response && response.statusCode);
+  
+  if (getResponseCode=='code') { 
+    console.log(response && response.statusCode);
+  }
+
+  if (showBody=="body") { 
+    console.log('body:', body);
+  }
+  if (showErrors=="error") { 
+    console.error('error:', error); 
+  }
+  if (error) { 
+    process.exit(1);
+  }
 });
